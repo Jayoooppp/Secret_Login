90da2a189dd7c08078d7ce36fd8738cf2e8a60a8
Jayoooppp <patel.js@somaiya.edu>
2022-07-21 13:59:00 +0530

    Added the heroku path rather than localhost path

--- app.js
+++ app.js
@@ -58,7 +58,7 @@ passport.deserializeUser(function(id, done) {
 passport.use(new GoogleStrategy({
     clientID: process.env.CLIENT_ID,
     clientSecret: process.env.CLIENT_SECRETE,
-    callbackURL: "http://localhost:3000/auth/google/secrets",
+    callbackURL: process.env.URL + "/auth/google/secrets",
     userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo"
   },
   function(accessToken, refreshToken, profile, cb) {
@@ -88,7 +88,7 @@ passport.use(new GoogleStrategy({
 passport.use(new FacebookStrategy({
     clientID: process.env.APP_ID,
     clientSecret: process.env.APP_SECRET,
-    callbackURL: "http://localhost:3000/auth/facebook/secrets",
+    callbackURL: process.env.URL + "/auth/facebook/secrets",
     enableProof: true
   },
   function(accessToken, refreshToken, profile, cb) {
